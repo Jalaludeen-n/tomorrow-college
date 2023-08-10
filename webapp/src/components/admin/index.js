8371dc556e6c5f09cedaf2c84c328ed43220baac
Bug-programmer <jalaludeen.n@yahoo.com>
2023-08-10 13:49:45 +0530

    Data formation

--- webapp/src/components/admin/index.js
+++ webapp/src/components/admin/index.js
@@ -4,152 +4,163 @@ import RolePDFUpload from './roles';
 import sendDataToAirtable from '../services/airtable';
 
 const Index = () => {
-  // State for the first half of the form
-  const [gameName, setGameName] = useState('');
-  const [numRounds, setNumRounds] = useState('');
-  const [excelLink, setExcelLink] = useState('');
-  const [resultsSubmission, setResultsSubmission] = useState('');
-  const [scoreVisibility, setScoreVisibility] = useState('');
-  const [allowAutoSelection, setAllowAutoSelection] = useState(false);
-  const [individualInstructions, setIndividualInstructions] = useState(false);
-  const [formData, setFormData] = useState(new FormData());
+// State for the first half of the form
+const [gameName, setGameName] = useState('');
+const [numRounds, setNumRounds] = useState(0);
+const [excelLink, setExcelLink] = useState('');
+const [resultsSubmission, setResultsSubmission] = useState('');
+const [scoreVisibility, setScoreVisibility] = useState('');
+const [allowAutoSelection, setAllowAutoSelection] = useState(false);
+const [individualInstructions, setIndividualInstructions] = useState(false);
+const [formData, setFormData] = useState(new FormData());
 
-  const [levels, setLevels] = useState([]);
-  const pdfCount = 3; // Define the pdfCount as needed
+const [levels, setLevels] = useState([]);
 
-  const handleFormDataChange = (newFormData) => {
-    setFormData(...newFormData);
-   
-  };
-
-
-
-  const handleSubmit = async () => {
-    const formattedData = formatDataForAirtable(levels); // Format your data
-    console.log(formattedData)
-
-    // try {
-    //   await sendDataToAirtable(formattedData); // Send data to Airtable
-    //   console.log('Data successfully sent to Airtable');
-    // } catch (error) {
-    //   console.error('Error sending data to Airtable:', error);
-    // }
-  };
-
-  // Format your data according to Airtable schema
-  const formatDataForAirtable = (levels) => {
-    // Format levels data as needed
+const handleFormDataChange = (newFormData) => {
+  setFormData(...newFormData);
   
-    const formattedData = levels.flatMap((level) =>
-  level.pdfs.map((pdf,index) => ({
-    fields:{
-    GameID: "1234",
-    Role: level.role,
-    LevelDescription: pdf,
-    Level: index+1,
-    }
-  }))
-);
-
-    return formattedData;
-  };
-
-  // Dropdown options
-  const resultsSubbmision = [
-    { value: '', label: 'Results subbmision' },
-    { value: 'Each member does  their own subbmision', label: 'Each member does  their own subbmision' },
-    { value: 'Each group member can submit  group answer', label: 'Each group member can submit  group answer' },
-    { value: 'Only one peson can submit group answer', label: 'Only one peson can submit group answer' },
-  ];
-  const scoreVisibilityForPlayers = [
-    { value: '', label: 'Score visibility for players' },
-    { value: 'See others score', label: 'See others score' },
-    { value: 'See score per round', label: 'See score per round' },
-    { value: 'See score only in the end', label: 'See score only in the end' },
-  ];
+};
 
 
-  // Event handlers
-  const handleDropdownChange = (event, setStateFunction) => {
-    setStateFunction(event.target.value);
-  };
 
-  const handleCheckboxChange = (setStateFunction) => {
-    setStateFunction((prevState) => !prevState);
-  };
+const handleSubmit = async () => {
+  const formattedData = formatDataForAirtable(levels);
+  console.log(formattedData) // Format your data
 
-  return (
-    <>
-    <div className='container'>
-    <div className='half'>
-      <h2>Game Settings</h2>
-      <div className='input-group'>
-        <label>Game Name:</label>
-        <input type='text' value={gameName} onChange={(e) => setGameName(e.target.value)} />
-      </div>
-      <div className='input-group'>
-        <label>Number of Rounds:</label>
-        <input
-          type='number'
-          value={numRounds}
-          onChange={(e) => setNumRounds(e.target.value)}
-        />
-      </div>
-      <div className='input-group'>
-        <label>Link to Excel:</label>
-        <input type='text' value={excelLink} onChange={(e) => setExcelLink(e.target.value)} />
-      </div>
-      <div className='input-group'>
-        <label>Results Submission:</label>
-        <select
-          value={resultsSubmission}
-          onChange={(e) => handleDropdownChange(e, setResultsSubmission)}
-        >
-          {resultsSubbmision.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className='input-group'>
-        <label>Score Visibility for Players:</label>
-        <select
-          value={scoreVisibility}
-          onChange={(e) => handleDropdownChange(e, setScoreVisibility)}
-        >
-          {scoreVisibilityForPlayers.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className='checkbox-group'>
-        <input
-          type='checkbox'
-          checked={allowAutoSelection}
-          onChange={() => handleCheckboxChange(setAllowAutoSelection)}
-        />
-        <label>Allow for auto selection role</label>
-      </div>
-      <div className='checkbox-group'>
-        <input
-          type='checkbox'
-          checked={individualInstructions}
-          onChange={() => handleCheckboxChange(setIndividualInstructions)}
-        />
-        <label>Individual instructions per round</label>
-      </div>
+  try {
+    await sendDataToAirtable(formattedData); // Send data to Airtable
+    console.log('Data successfully sent to Airtable');
+  } catch (error) {
+    console.error('Error sending data to Airtable:', error);
+  }
+};
+
+// Format your data according to Airtable schema
+const formatDataForAirtable = (levels) => {
+  const formData = new FormData();
+
+  formData.append(`data`, JSON.stringify({
+    GameName: gameName,
+    NumberOfRounds : numRounds,
+    GoogleSheet : excelLink,
+    ResultsSubbmision : resultsSubmission,
+    ScoreVisibility : scoreVisibility,
+    RoleSelection : allowAutoSelection,
+    IndividualInstructions : individualInstructions,
+
+
+  }));
+
+  const roles = [];
+// Create FormData and append each data entry separately
+levels.forEach((level) => {
+level.pdfs.forEach((pdf) => {
+  formData.append("pdf", pdf);
+});
+roles.push(level.role);
+});
+formData.append("roles",roles)
+  return formData;
+};
+
+  
+
+// Dropdown options
+const resultsSubbmision = [
+  { value: '', label: 'Results subbmision' },
+  { value: 'Each member does  their own subbmision', label: 'Each member does  their own subbmision' },
+  { value: 'Each group member can submit  group answer', label: 'Each group member can submit  group answer' },
+  { value: 'Only one peson can submit group answer', label: 'Only one peson can submit group answer' },
+];
+const scoreVisibilityForPlayers = [
+  { value: '', label: 'Score visibility for players' },
+  { value: 'See others score', label: 'See others score' },
+  { value: 'See score per round', label: 'See score per round' },
+  { value: 'See score only in the end', label: 'See score only in the end' },
+];
+
+
+// Event handlers
+const handleDropdownChange = (event, setStateFunction) => {
+  setStateFunction(event.target.value);
+};
+
+const handleCheckboxChange = (setStateFunction) => {
+  setStateFunction((prevState) => !prevState);
+};
+
+return (
+  <>
+  <div className='container'>
+  <div className='half'>
+    <h2>Game Settings</h2>
+    <div className='input-group'>
+      <label>Game Name:</label>
+      <input type='text' value={gameName} onChange={(e) => setGameName(e.target.value)} />
     </div>
-    <div className='half'>
-    <RolePDFUpload levels={levels} setLevels={setLevels} pdfCount={pdfCount} onFormDataChange={handleFormDataChange}/>
-         
-      </div>
-
+    <div className='input-group'>
+      <label>Number of Rounds:</label>
+      <input
+        type='number'
+        value={numRounds}
+        onChange={(e) => setNumRounds(e.target.value)}
+      />
+    </div>
+    <div className='input-group'>
+      <label>Link to Excel:</label>
+      <input type='text' value={excelLink} onChange={(e) => setExcelLink(e.target.value)} />
+    </div>
+    <div className='input-group'>
+      <label>Results Submission:</label>
+      <select
+        value={resultsSubmission}
+        onChange={(e) => handleDropdownChange(e, setResultsSubmission)}
+      >
+        {resultsSubbmision.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+    <div className='input-group'>
+      <label>Score Visibility for Players:</label>
+      <select
+        value={scoreVisibility}
+        onChange={(e) => handleDropdownChange(e, setScoreVisibility)}
+      >
+        {scoreVisibilityForPlayers.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+    <div className='checkbox-group'>
+      <input
+        type='checkbox'
+        checked={allowAutoSelection}
+        onChange={() => handleCheckboxChange(setAllowAutoSelection)}
+      />
+      <label>Allow for auto selection role</label>
+    </div>
+    <div className='checkbox-group'>
+      <input
+        type='checkbox'
+        checked={individualInstructions}
+        onChange={() => handleCheckboxChange(setIndividualInstructions)}
+      />
+      <label>Individual instructions per round</label>
+    </div>
   </div>
-        <button onClick={handleSubmit}> submit</button>
-        </>
+  <div className='half'>
+  <RolePDFUpload levels={levels} setLevels={setLevels} pdfCount={numRounds} resultsSubmission={resultsSubmission} individualInstructions = {individualInstructions}/>
+        
+    </div>
+
+</div>
+      <button onClick={handleSubmit}> submit</button>
+      </>
 
 );
 };
--- webapp/src/components/admin/roles.js
+++ webapp/src/components/admin/roles.js
@@ -1,48 +1,68 @@
 import React, { useState } from 'react';
 
-const RolePDFUpload = ({ levels, setLevels, pdfCount,onFormDataChange }) => {
-  const handleAddLevel = () => {
-    setLevels([...levels, { role: '', pdfs: Array.from({ length: pdfCount }, () => null) }]);
-  };
-  const formData = new FormData();
+const RolePDFUpload = ({ levels, setLevels, pdfCount,resultsSubmission ,individualInstructions}) => {
+const handleAddLevel = () => {
+  setLevels([...levels, { role: '', pdfs: Array.from({ length: pdfCount }, () => null) }]);
+};
+const formData = new FormData();
 
-  const handleRoleChange = (index, value) => {
-    const newLevels = [...levels];
-    newLevels[index].role = value;
-    setLevels(newLevels);
-  };
+const handleRoleChange = (index, value) => {
+  const newLevels = [...levels];
+  newLevels[index].role = value;
+  setLevels(newLevels);
+};
 
-  const handlePDFChange = (levelIndex, pdfIndex, file) => {
-    const newLevels = [...levels];
-    newLevels[levelIndex].pdfs[pdfIndex] = new Blob([file], { type: "application/pdf" });
-    setLevels(newLevels); 
-  };
+const handlePDFChange = (levelIndex, pdfIndex, file) => {
+  const newLevels = [...levels];
+  const uniqueFilename = generateUniqueFilename(newLevels[levelIndex].role, pdfIndex, ".pdf");
+  const modifiedFile = new File([file], uniqueFilename, { type: "application/pdf" });
+  console.log(modifiedFile)
   
-  return (
-    <div className="role-pdf-upload">
-      {levels.map((level, levelIndex) => (
-        <div key={levelIndex} className="level">
+  newLevels[levelIndex].pdfs[pdfIndex] = modifiedFile;
+  setLevels(newLevels); 
+};
+
+const generateUniqueFilename = (role, pdfIndex, originalFilename) => {
+  const sanitizedRole = role.replace(/\s+/g, "_"); // Replace spaces with underscores
+  const extension = originalFilename.split('.').pop();
+  return `${sanitizedRole}_Level${pdfIndex + 1}.${extension}`;
+};
+
+return (
+  <div className="role-pdf-upload">
+    {levels.map((level, levelIndex) => (
+      <div key={levelIndex} className="level">
+        <input
+          type="text"
+          value={level.role}
+          onChange={(e) => handleRoleChange(levelIndex, e.target.value)}
+          placeholder={`Role`}
+        />
+        { individualInstructions && level.pdfs.map((pdf, pdfIndex) => (
           <input
-            type="text"
-            value={level.role}
-            onChange={(e) => handleRoleChange(levelIndex, e.target.value)}
-            placeholder={`Role for Level ${levelIndex + 1}`}
+            key={pdfIndex}
+            type="file"
+            accept=".pdf"
+            onChange={(e) => handlePDFChange(levelIndex, pdfIndex, e.target.files[0])}
           />
-          {level.pdfs.map((pdf, pdfIndex) => (
-            <input
-              key={pdfIndex}
-              type="file"
-              accept=".pdf"
-              onChange={(e) => handlePDFChange(levelIndex, pdfIndex, e.target.files[0])}
-            />
-          ))}
-        </div>
-      ))}
-      <button className="add-level-button" onClick={handleAddLevel}>
-        Add Level
-      </button>
-    </div>
-  );
+        ))}
+{<div className='checkbox-group'>
+      <input
+        type='checkbox'
+        // checked={individualInstructions}
+        // onChange={() => handleCheckboxChange(setIndividualInstructions)}
+      />
+      <label>This role can only submit the group result</label>
+    </div>}
+
+      </div>
+    )
+    )}
+    <button className="add-level-button" onClick={handleAddLevel}>
+      Add Role
+    </button>
+  </div>
+);
 };
 
 export default RolePDFUpload;
