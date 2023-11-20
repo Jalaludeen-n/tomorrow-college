c224eada95e7fe1f1f98466a7f1e07784c9bdd69
Bug-programmer <jalaludeen.n@yahoo.com>
2023-11-21 01:44:26 +0530

    Added loader

--- webapp/src/components/NabarRight/Decision/index.js
+++ webapp/src/components/NabarRight/Decision/index.js
@@ -4,14 +4,17 @@ import { Row, Form, Col } from "react-bootstrap";
 import { encryptData, isObjectEmpty } from "../../helper/utils";
 import { fetchQustions, storeAnsweres } from "../../services/decision";
 import { useNavigate } from "react-router-dom";
+import Loader from "../../../pages/Loader";
 
 const Decision = ({ data }) => {
   const navigate = useNavigate();
 
   const [questions, setQustions] = useState([]);
   const [answers, setAnswers] = useState([]);
+  const [loader, setLoader] = useState(false);
 
   const fetchLevelDetailsAndSet = async (data) => {
+    setLoader(true);
     const formData = {
       level: data.level,
       sheetID: data.GoogleSheetID,
@@ -20,7 +23,9 @@ const Decision = ({ data }) => {
     try {
       const res = await fetchQustions(formData);
       setQustions(res.data);
+      setLoader(false);
     } catch (error) {
+      setLoader(false);
       console.error("Error fetching questions:", error);
     }
   };
@@ -38,7 +43,7 @@ const Decision = ({ data }) => {
   };
 
   const handleSubmit = async (event) => {
-    // setLoader(true);
+    setLoader(true);
     const formData = new FormData();
     event.preventDefault();
 
@@ -57,90 +62,101 @@ const Decision = ({ data }) => {
     );
 
     const res = await storeAnsweres(formData);
+    console.log(res);
+    console.log("desic");
     const updatedData = {
       ...data,
       level: res.data.data.CurrentLevel,
       started: res.data.data.started,
     };
-    console.log(updatedData);
-
+    setLoader(true);
     const encryptedData = encryptData(updatedData, "secret_key");
     navigate(`/result?data=${encodeURIComponent(encryptedData)}`);
   };
 
   return (
     <div className={styles.decision_container}>
-      <Row className={`${styles.decision_header} `}>Decisions</Row>
-      <Form onSubmit={handleSubmit}>
-        <Col className={`${styles.questionsContainer}`}>
-          {questions.length > 0 &&
-            questions.map((question, index) => (
-              <div key={index} className={`question ${styles.question}`}>
-                <p>{question.question}</p>
-                {question.type === "Multiple-Choice" && (
-                  <Form.Group
-                    className={`options ${styles.options}`}
-                    aria-required>
-                    {question.choices.map((option, optionIndex) => (
-                      <Form.Check
-                        key={optionIndex}
-                        type='radio'
-                        name={`question-${index}`}
-                        label={option}
-                        value={option}
-                        onChange={(e) =>
-                          handleRadioChange(index, e.target.value)
-                        }
-                        checked={answers[index] === option}
-                        required
-                      />
-                    ))}
-                  </Form.Group>
-                )}
-                {question.type === "Boolean" && (
-                  <Form.Group className={`input ${styles.input}`} required>
-                    <Form.Check
-                      type='radio'
-                      name={`question-${index}`}
-                      label='True'
-                      value='true'
-                      onChange={() => handleRadioChange(index, "true")}
-                      checked={answers[index] === "true"}
-                      inline
-                      required
-                    />
-                    <Form.Check
-                      type='radio'
-                      name={`question-${index}`}
-                      label='False'
-                      value='false'
-                      checked={answers[index] === "false"}
-                      onChange={() => handleRadioChange(index, "false")}
-                      inline
-                    />
-                  </Form.Group>
-                )}
-                {question.type === "Number" && (
-                  <Form.Group className={`input ${styles.input}`}>
-                    <Form.Control
-                      type='Number'
-                      name={`question-${index}`}
-                      className={`form-control ${styles.numberInput}`}
-                      value={answers[index] || ""}
-                      onChange={(e) => handleRadioChange(index, e.target.value)}
-                      required
-                    />
-                  </Form.Group>
-                )}
-              </div>
-            ))}
-        </Col>
-        <div className={styles.startButtonContainer}>
-          <button className={styles.startButton} type='submit'>
-            Submit
-          </button>
-        </div>
-      </Form>
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <Row className={`${styles.decision_header} `}>Decisions</Row>
+          <Form onSubmit={handleSubmit}>
+            <Col className={`${styles.questionsContainer}`}>
+              {questions.length > 0 &&
+                questions.map((question, index) => (
+                  <div key={index} className={`question ${styles.questions}`}>
+                    <Form.Label className={`question ${styles.question}`}>
+                      {question.question}
+                    </Form.Label>
+                    {question.type === "Multiple-Choice" && (
+                      <Form.Group
+                        className={`options ${styles.options}`}
+                        aria-required>
+                        {question.choices.map((option, optionIndex) => (
+                          <Form.Check
+                            key={optionIndex}
+                            type='radio'
+                            name={`question-${index}`}
+                            label={option}
+                            value={option}
+                            onChange={(e) =>
+                              handleRadioChange(index, e.target.value)
+                            }
+                            checked={answers[index] === option}
+                            required
+                          />
+                        ))}
+                      </Form.Group>
+                    )}
+                    {question.type === "Boolean" && (
+                      <Form.Group className={`input ${styles.input}`} required>
+                        <Form.Check
+                          type='radio'
+                          name={`question-${index}`}
+                          label='True'
+                          value='true'
+                          onChange={() => handleRadioChange(index, "true")}
+                          checked={answers[index] === "true"}
+                          inline
+                          required
+                        />
+                        <Form.Check
+                          type='radio'
+                          name={`question-${index}`}
+                          label='False'
+                          value='false'
+                          checked={answers[index] === "false"}
+                          onChange={() => handleRadioChange(index, "false")}
+                          inline
+                        />
+                      </Form.Group>
+                    )}
+                    {question.type === "Number" && (
+                      <Form.Group className={`input ${styles.input}`}>
+                        <Form.Control
+                          type='Number'
+                          name={`question-${index}`}
+                          className={`form-control ${styles.numberInput}`}
+                          value={answers[index] || ""}
+                          onChange={(e) =>
+                            handleRadioChange(index, e.target.value)
+                          }
+                          required
+                        />
+                      </Form.Group>
+                    )}
+                  </div>
+                ))}
+            </Col>
+            <div className={styles.startButtonContainer}>
+              <button className={styles.startButton} type='submit'>
+                Submit
+              </button>
+            </div>
+          </Form>
+        </>
+      )}
     </div>
   );
 };
